48b6455816f5b519783d66ede0c9ab25e09f0732
Mo_Work <hamudal.hd@gmail.com>
2024-02-05 16:04:40 +0100

    V2 non func

--- TestGround_&_Prototype/TestGround/Test_V7/Test_Test/Test_Dario/Polygon_By_Postal_Code_Extractor_&_Connection_V8_Test/V8_Dario/Leaflet_V2.py
+++ TestGround_&_Prototype/TestGround/Test_V7/Test_Test/Test_Dario/Polygon_By_Postal_Code_Extractor_&_Connection_V8_Test/V8_Dario/Leaflet_V2.py
@@ -1,8 +1,7 @@
 import json
 import geopandas as gpd
-from dash import Dash, html, dcc, callback, Output, Input
+from dash import Dash, html, dcc, callback, Output, Input, State
 import dash_leaflet as dl
-from dash_extensions.javascript import assign
 
 # Replace the actual file path with your GeoJSON file location
 geojson_file_path = r"C:\Users\Admin\OneDrive\Dokumente\Projects\Retoflow_Filter-1\TestGround_&_Prototype\TestGround\Test_V7\Test_Test\Test_Dario\Polygon_By_Postal_Code_Extractor_&_Connection_V8_Test\V8_Dario\Germany_postal_codes.geojson"
@@ -31,16 +30,16 @@ app.layout = html.Div([
                 data=polygons_geojson,
                 id='polygons',
                 style={'color': 'blue'},
-                hoverStyle=assign("{'fillColor': 'yellow', 'fillOpacity': 0.2}"),
-                hideout=dict(type='FeatureCollection', features=[]),  # Initialize hideout
+                hoverStyle={'fillColor': 'yellow', 'fillOpacity': 0.2},
+                hideout=dict(type='FeatureCollection', features=[]),
             ),
             # Postal code borders with multipolygons in red
             dl.GeoJSON(
                 data=multipolygons_geojson,
                 id='multipolygons',
                 style={'color': 'red'},
-                hoverStyle=assign("{'fillColor': 'orange', 'fillOpacity': 0.2}"),
-                hideout=dict(type='FeatureCollection', features=[]),  # Initialize hideout
+                hoverStyle={'fillColor': 'orange', 'fillOpacity': 0.2},
+                hideout=dict(type='FeatureCollection', features=[]),
             ),
         ],
         center=[51.1657, 10.4515],  # Center of Germany
@@ -66,43 +65,38 @@ app.layout = html.Div([
         Output('info-panel', 'children'),
         Output('polygons', 'hideout'),
         Output('multipolygons', 'hideout'),
-        Output('polygons', 'hideout'),
-        Output('multipolygons', 'hideout'),
     ],
     [
-        Input('polygons', 'featureHover'),
-        Input('multipolygons', 'featureHover'),
-        Input('layer-control', 'value')
+        Input('polygons', 'featureClick'),
+        Input('multipolygons', 'featureClick'),
+        Input('layer-control', 'value'),
+    ],
+    [
+        State('polygons', 'hideout'),
+        State('multipolygons', 'hideout'),
     ]
 )
-def update_info_panel(polygons_hover, multipolygons_hover, selected_layers):
-    # Initialize hideouts
-    polygons_hideout = dict(type='FeatureCollection', features=[])
-    multipolygons_hideout = dict(type='FeatureCollection', features=[])
+def update_info_panel(polygons_click, multipolygons_click, layers, polygons_hideout, multipolygons_hideout):
+    selected_layer = []
+    if 'polygons' in layers:
+        selected_layer.append(polygons_geojson)
+    if 'multipolygons' in layers:
+        selected_layer.append(multipolygons_geojson)
 
-    if 'polygons' in selected_layers:  # Show polygons
-        polygons_hideout['features'] = polygons_geojson['features']  # Enable polygons
-    else:  # Hide polygons
-        pass  # Hideout already initialized with empty features
+    selected_hideout = polygons_hideout if 'polygons' in layers else multipolygons_hideout
 
-    if 'multipolygons' in selected_layers:  # Show multipolygons
-        multipolygons_hideout['features'] = multipolygons_geojson['features']  # Enable multipolygons
-    else:  # Hide multipolygons
-        pass  # Hideout already initialized with empty features
-
-    # Update information panel based on hover events
-    if polygons_hover:
-        feature = polygons_hover[0]
-        postal_code = feature['properties'].get('postal_code', 'N/A')
-        return f"Postal Code: {postal_code}", polygons_hideout, multipolygons_hideout, {}, {}
-    elif multipolygons_hover:
-        feature = multipolygons_hover[0]
-        postal_code = feature['properties'].get('postal_code', 'N/A')
-        return f"Postal Code: {postal_code}", polygons_hideout, multipolygons_hideout, {}, {}
+    if polygons_click:
+        feature = polygons_click['properties']
+        postal_code = feature.get('postal_code', 'N/A')
+        selected_hideout['features'] = [poly for poly in polygons_geojson['features'] if poly['properties'] == feature]  # Update hideout
+        return f"Postal Code: {postal_code}", selected_hideout, selected_layer
+    elif multipolygons_click:
+        feature = multipolygons_click['properties']
+        postal_code = feature.get('postal_code', 'N/A')
+        selected_hideout['features'] = [multi for multi in multipolygons_geojson['features'] if multi['properties'] == feature]  # Update hideout
+        return f"Postal Code: {postal_code}", selected_hideout, selected_layer
     else:
-        return "Hover over a feature to see details.", polygons_hideout, multipolygons_hideout, {}, {}
-
-
+        return "Click on a feature to see details.", selected_hideout, selected_layer
 
 if __name__ == '__main__':
     app.run_server(debug=True)
